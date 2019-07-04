f24a4bf34caef47dce7843462aeb6562f1f10083
于广超 <yugc1989@gmail.com>
2019-07-04 15:57:13 +0800

    更新axios 扩展功能

---
+++ examples/extend/app.ts
@@ -0,0 +1,9 @@
+import axios from '../../src/index'
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
---
+++ src/helpers/error.ts
@@ -0,0 +1,34 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  isAxiosError: boolean = false
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  const error = new AxiosError(message, config, code, request, response)
+  return error
+}

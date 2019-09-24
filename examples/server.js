10685c4b22c9ba2644695fc8a6ee626d6b4cc87b
于广超 <yugc1989@gmail.com>
2019-09-24 16:45:08 +0800

    添加拦截器支持

--- examples/server.js
+++ examples/server.js
@@ -8,13 +8,15 @@ const WebpackConfig = require('./webpack.config')
 const app = express()
 const compiler = webpack(WebpackConfig)
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
 
 app.use(webpackHotMiddleware(compiler))
 
@@ -34,6 +36,8 @@ registerErrorRouter()
 
 registerExtendRouter()
 
+registerInterceptorRouter()
+
 app.use(router)
 
 const port = process.env.PORT || 8080
@@ -41,7 +45,7 @@ module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
 
-function registerSimpleRouter () {
+function registerSimpleRouter() {
   router.get('/simple/get', function(req, res) {
     res.json({
       msg: `hello world`
@@ -49,7 +53,7 @@ function registerSimpleRouter () {
   })
 }
 
-function registerBaseRouter () {
+function registerBaseRouter() {
   router.get('/base/get', function(req, res) {
     res.json(req.query)
   })
@@ -60,7 +64,7 @@ function registerBaseRouter () {
 
   router.post('/base/buffer', function(req, res) {
     let msg = []
-    req.on('data', (chunk) => {
+    req.on('data', chunk => {
       if (chunk) {
         msg.push(chunk)
       }
@@ -72,7 +76,7 @@ function registerBaseRouter () {
   })
 }
 
-function registerErrorRouter () {
+function registerErrorRouter() {
   router.get('/error/get', function(req, res) {
     if (Math.random() > 0.5) {
       res.json({
@@ -93,7 +97,7 @@ function registerErrorRouter () {
   })
 }
 
-function registerExtendRouter () {
+function registerExtendRouter() {
   router.get('/extend/get', function(req, res) {
     res.json({
       msg: 'hello world'
@@ -136,3 +140,8 @@ function registerExtendRouter () {
   })
 }
 
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function(req, res) {
+    res.end('hello')
+  })
+}
